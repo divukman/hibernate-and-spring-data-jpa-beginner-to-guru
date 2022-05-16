b439a8aab5e1e2df318295a35443ca890547dcb2
dimitar <dimitar.vukman@bluedropmedical.com>
2022-05-16 18:03:31 +0200

    Initializing project

---
+++ sdjpa-int/src/main/java/tech/dimitar/rosdjpaint/SdjpaIntApplication.java
@@ -0,0 +1,16 @@
+package tech.dimitar.rosdjpaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Data JPA
+ */
+@SpringBootApplication
+public class SdjpaIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaIntApplication.class, args);
+	}
+
+}
---
+++ sdjpa-int/src/test/java/tech/dimitar/rosdjpaint/SdjpaIntApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.rosdjpaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
